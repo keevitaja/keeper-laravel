7ea5dc4428adf2c0a1e3644318b38612b6f3cdb4
Tanel Tammik <keevitaja@gmail.com>
2014-02-23 17:48:55 +0200

    deleted some files

--- src/Keevitaja/Keeper/Facades/Keeper.php
+++
@@ -1,8 +0,0 @@
-<?php namespace Keevitaja\Keeper\Facades;
-
-use Illuminate\Support\Facades\Facade;
-
-class Keeper extends Facade {
-
-	protected static function getFacadeAccessor() { return 'keeper'; }
-}
